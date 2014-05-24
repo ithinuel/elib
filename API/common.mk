41d60050a31470f4f42310461ec34f407f35c2ba
Wilfried Chauveau <ithinuel@gmail.com>
2014-05-24 23:03:10 +0200

    Add license notice.
    Signed-off-by: Wilfried Chauveau <ithinuel@gmail.com>

--- API/common.mk
+++ API/common.mk
@@ -1,3 +1,17 @@
+#	Copyright 2014 Chauveau Wilfried
+#
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#		 http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
+
 ###################################
 ## project variables
 
@@ -89,5 +103,5 @@ end:
 	@echo $(MSG_END)
 	
 tests: elf
-	@./$(OUT_DIR)/$(PROJECT).elf
+	@./$(OUT_DIR)/$(PRJ_NAME).elf
 
